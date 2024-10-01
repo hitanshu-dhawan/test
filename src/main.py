ffb697961ac698a7ea152cb579139dba6df06f3c
Hitanshu Dhawan <hitanshudhawan1996@gmail.com>
2024-10-01 16:39:32 +0530

    .

---
+++ src/main.py
@@ -0,0 +1,12 @@
+import os
+
+# print environment variable $CI
+
+# echo $CI
+# os.system("CI=false")
+# os.system("echo $CI")
+
+# os.environ["CI"] = "qwerty"
+print("....................")
+print(os.environ["CI"])
+print("....................")
